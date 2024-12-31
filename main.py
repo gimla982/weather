0ef07bd0687937132abd760da0f80f36491271e5
gimla982 <info@gimla.info>
2024-12-31 11:54:49 +0200

    new code

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import streamlit as st
+import requests
 
+def get_weather_data(city, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    return response.json()
 
-st.markdown ("""
+api_key = "ac2261df167c06bd38f816369574afa5"
+city = input("Enter city name: ")
+weather_data = get_weather_data(city, api_key)
 
-# Title
-## Subtitle
-
-- Bullet1
-- Bullet2
-- Bullet3
-         
-> Amazing Quote
-         
-""")
-
-st.radio("Which desert is best?", ["cake", "Ice Cream", "Pie" ])
+temperature = weather_data['main']['temp']
+weather_condition = weather_data['weather'][0]['description']
+humidity = weather_data['main']['humidity']
+print(f"Temperature: {temperature}°C\nWeather: {weather_condition}\nHumidity: {humidity}%")
